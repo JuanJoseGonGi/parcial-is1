24cd5906b107c680434e84d21ff8cb228b59a43e
yeraldinarboleda <yeraldin.1701811368@ucaldas.edu.co>
2021-07-23 21:07:21 -0500

    comentarios

--- src/ImpresoraDao.java
+++ src/ImpresoraDao.java
@@ -10,24 +10,24 @@ import java.util.List;
 
 public class ImpresoraDao implements Dao<Impresora> {
     private List<Impresora> impresoras = new ArrayList<>();
-
+ // metodo que busca una impreso por su id
     @Override
     public Impresora consultar(int id) {
         return impresoras.get(id);
     }
-
+// metodo que guarda una impresora
     @Override
     public void guardar(Impresora impresora) {
         impresora.setId(impresoras.size());
         impresoras.add(impresora);
     }
-
+//metodo que busca todas las impresoras
     @Override
     public List<Impresora> consultarTodos() {
         return impresoras;
 
     }
-
+//metodo que actualiza una impresora
     @Override
     public void actualizar(Impresora impresora, String[] params) {
         impresora.setSerial(params[0]);
@@ -36,7 +36,7 @@ public class ImpresoraDao implements Dao<Impresora> {
 
         impresoras.add(impresora);
     }
-
+// metodo que borra una impresora
     @Override
     public void borrar(Impresora impresora) {
         impresoras.remove(impresora);

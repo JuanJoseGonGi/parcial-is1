65e401b6c2a549e210d9dfa7926c504a7ee74482
Juan José González Giraldo <juan.1701716389@ucaldas.edu.co>
2021-07-23 19:56:43 -0500

    Create class GestionImpresoras and add logic

--- src/DatosImpresoras.java
+++ src/DatosImpresoras.java
@@ -1,3 +1,17 @@
-public class DatosImpresoras {
+import java.util.List;
 
+public class DatosImpresoras {
+  private Dao<Impresora> impresoraDao;
+
+  public List<Impresora> consultarImpresoras() {
+    return impresoraDao.consultarTodos();
+  }
+
+  public Impresora buscarImpresora(String serial) {
+    return impresoraDao.consultar(serial);
+  }
+
+  public void añadirImpresora(String serial, String marca, Boolean escaner) {
+    impresoraDao.guardar(new Impresora(serial, marca, escaner));
+  }
 }
--- src/Impresora.java
+++ src/Impresora.java
@@ -1,3 +1,42 @@
 public class Impresora {
+  private String serial;
+  private String marca;
+  private Boolean escaner;
 
+  public Impresora() {
+  }
+
+  public Impresora(String serial, String marca, Boolean escaner) {
+    this.serial = serial;
+    this.marca = marca;
+    this.escaner = escaner;
+  }
+
+  public String getSerial() {
+    return serial;
+  }
+
+  public void setSerial(String serial) {
+    this.serial = serial;
+  }
+
+  public String getMarca() {
+    return marca;
+  }
+
+  public void setMarca(String marca) {
+    this.marca = marca;
+  }
+
+  public Boolean getEscaner() {
+    return escaner;
+  }
+
+  public void setEscaner(Boolean escaner) {
+    this.escaner = escaner;
+  }
+
+  public String toString() {
+    return "Impresora [serial=" + serial + ", marca=" + marca + ", escaner=" + escaner + "]";
+  }
 }

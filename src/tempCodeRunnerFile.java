f7b0491e955d32750429054ed4d7c7a8b0995ee1
Juan José González Giraldo <juan.1701716389@ucaldas.edu.co>
2021-07-23 21:19:28 -0500

    Delete tempCodeRunnerFile.java

--- src/tempCodeRunnerFile.java
+++
@@ -1,3 +0,0 @@
-    // datosImpresoras.añadirImpresora("001", "HP", false);
-    // datosImpresoras.añadirImpresora("002", "HP", false);
-    // datosImpresoras.añadirImpresora("003", "Panasonic", true);
